ed0432d34d6b4a6de799ba700fb6dc7bbc300c43
francisco <kiko.fccarvalho@gmail.com>
2017-11-17 12:35:19 -0200

    Iniciando Curso Html5

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso de HTML5</title>
+        <meta name ="description" content="Esta pagina web está sendo construida no cursa de HTML 5"/>
+        <meta name="viewport" content="width=device-width, inita-scale=1.0"/>
+<!--  FAVICON 
+        STYLE 
+        FONT-->
+    </head>
+    <body>
+        <header>
+            <img  title="Curso de HTML5" alt="[Curso de Hml5]" src="img/_logo.png"/>
+            <h1>Curso de Html5</h1>
+            <ul>
+                <li><a href="#home" title="Curso de Html5">Home</a></li>
+                <li><a href="#curso" title="sobre o Curso de Html5">O Curso</a></li>
+                <li><a  target="_blank" rel="follow"href="https://www.upinside.com.br" title="Curso World Series - Html5 do Jeito Certo">Work  Series</a></li>
+                <li><a href="#videoaulas" title="Confira Video Aulas do Curso de Html5">Video Aulas</a></li>
+                <li><a href="#contato" title="Contato sobre o Curso de Html5">Fale Conosco</a></li>
+            </ul>
+        </header>
+        <main>
+            
+        </main>
+        
+        <footer>
+            
+        </footer>
+    </body>
+</html>
